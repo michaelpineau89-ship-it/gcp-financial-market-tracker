50edaede84aa8515e5b4807436a6f01b1cf58bb7
Mike Pineau <mikepineau@Mikes-MacBook-Air.local>
2026-03-29 22:53:44 -0400

    feat: updated main

--- src/alphavantage/main.py
+++ src/alphavantage/main.py
@@ -1,7 +1,42 @@
+import requests
+from time import sleep
+import pandas_gbq
+import pandas as pd
+import os
 
+TARGET_TICKERS = [
+    # Mega-Cap Tech & AI
+    "AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "AMD", "AVGO", "PLTR",
+    # Financials & Canadian Banks
+    "JPM", "BRK-B", "BNS", "BMO", "RY", "HOOD", 
+    # Auto & Manufacturing
+    "TSLA", "TM", "GM",
+    # Healthcare & Cybersecurity
+    "JNJ", "LLY", "GEN",
+    # Crypto
+    "BTC-USD", "ETH-USD",
+    # Market Benchmarks
+    "SPY", "QQQ"
+]
+
+api_key = os.environ.get("API_KEY")
+
+def get_data(ticker, key):
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={key}'
+    r = requests.get(url)
+    return pd.Dataframe(r.json()['Global Quote'])
 
 def run():
-    raise NotImplemented
+    itter = 1
+    output_data = pd.DataFrame()
+
+    for ticker in TARGET_TICKERS:
+        data = get_data(ticker, api_key)
+        
+        itter += 1
+
+        if itter % 5 == 0:
+            sleep(60)
 
 if __name__ == "__main__":
     run()
